5cd4f6c3d383720dff64285d5fb5ab7c0ab1de37
Manuel Rivera <marp.developer@gmail.com>
2023-03-28 21:01:15 -0500

    gh pages

--- astro.config.ts
+++ astro.config.ts
@@ -6,5 +6,7 @@ import astroI18next from 'astro-i18next'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mukhuve.github.io',
+  base: '/website',
   integrations: [astroI18next(), compress(), mdx(), tailwind()],
 })
